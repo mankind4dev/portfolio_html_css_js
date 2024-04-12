a46d65b4f951ff041b2e8d090e2177ff17eec7ef
mankind4dev <ogunmakin120@gmail.com>
2024-04-12 16:03:44 +0100

    Nice work

---
+++ main.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".text", {
+  strings: ["Full Stack Developer", "iOS Developer", "Android Developer"],
+  typeSpeed: 100,
+  backSPeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
